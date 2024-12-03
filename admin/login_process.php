fca26399742dda09617d871d83edb63aac546ffc
Xkittipon <kittiponx@gmail.com>
2024-12-03 12:47:22 +0700

    add register page

--- admin/login_process.php
+++ admin/login_process.php
@@ -2,32 +2,23 @@
 include('../db.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password = md5($password);
 
     // คำสั่ง SQL
     $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
+        session_start();
         $user = $result->fetch_assoc();
-        $password = md5($password);
-
-        // ดีบักแสดงค่า
-        echo "รหัสผ่านที่เก็บในฐานข้อมูล: " . $user['password'] . "<br>";
-        echo "รหัสผ่านที่กรอก: " . $password . "<br>";
-
-        // ตรวจสอบรหัสผ่าน
-        if (password_verify($password, $user['password'])) {
-            session_start();
-            $_SESSION['username'] = $username;
+        $_SESSION['username'] = $username;
             header("Location: admin_panel.php");
-            exit;
-        } else {
-            echo "รหัสผ่านไม่ถูกต้อง";
-        }
+            exit();
+
     } else {
-        echo "ไม่พบผู้ใช้งาน";
+        echo "ไม่พบผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง";
     }
 }
 ?>
---
+++ admin/register.php
@@ -0,0 +1,16 @@
+<?php include('../db.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>สมัครบัญชีผู้ใช้งาน</title>
+</head>
+<body>
+    <h1>สมัครบัญชีผู้ใช้งาน</h1>
+    <form method="POST" action="register_process.php">
+        <input type="text" name="email" placeholder="อีเมล" required>
+        <input type="text" name="username" placeholder="ชื่อผู้ใช้" required>
+        <input type="password" name="password" placeholder="รหัสผ่าน" required>
+        <button type="submit">สมัคร</button>
+    </form>
+</body>
+</html>
