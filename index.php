d94e102eb985cc8ea67dd6906040a736da02bd23
apsn1 <137771397+apsn1@users.noreply.github.com>
2024-12-06 10:10:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,131 +23,30 @@
 </head>
 
 <body>
-    
+
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
         <div class="container">
             <a href="############################################">
                 <img class="logoinbanner" src="admin/img/logo/logoOld1.png"
                     style="height: 75px; width: 97px; margin-right: 50px;" alt="Logo"></a>
-                <!-- <a class="navbar-brand" href="#page-top">One Siame</a>-->
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary1 text-white rounded"
-                    type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                                href="/templates/index.html">หน้าแรก</a></li>
-                        <li class="nav-item mx-0 mx-lg-1 dropdown">
-                            <a class="nav-link py-3 px-0 px-lg-3 rounded dropdown-toggle" href="#Home"
-                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                หมวดหมู่สินค้า
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#option1">ออกแบบอาคาร</a></li>
-                                <li><a class="dropdown-item" href="#option2">ฟาซาดแบบอะลูมิเนียม</a></li>
-                                <li><a class="dropdown-item" href="#option3">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option4">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option5">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option6">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option7">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option8">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option9">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option10">ฟาซาดแบบโมเดิร์น</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item mx-0 mx-lg-1 dropdown">
-                            <a class="nav-link py-3 px-0 px-lg-3 rounded dropdown-toggle" href="#Home"
-                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                หน้ากากอาคาร - ฟาซาด
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#option1">ออกแบบอาคาร</a></li>
-                                <li><a class="dropdown-item" href="#option2">ฟาซาดแบบอะลูมิเนียม</a></li>
-                                <li><a class="dropdown-item" href="#option3">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option4">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option5">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option6">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option7">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option8">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option9">ฟาซาดแบบโมเดิร์น</a></li>
-                                <li><a class="dropdown-item" href="#option10">ฟาซาดแบบโมเดิร์น</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                                href="/templates/page4.html">เกี่ยวกับเรา</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                                href="/templates/page6.html">ติดต่อเรา</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                                href="#project">โปรเจค</a></li>
-                                <li class="nav-item mx-0 mx-lg-1 dropdown">
-                                    <a class="nav-link py-3 px-0 px-lg-3 rounded dropdown-toggle" href="/templates/page5.html"
-                                        id="navbar1Dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        บทความ
-                                    </a>
-        
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a class="dropdown-item" href="#option2">บทความฟาซาดคลีนิค</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/blogคลินิก.html">วงการแพทย์ต้องสั่นสะเทือน</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/blog-Lisa.html">7 ร้านที่ลิซ่าพาเยือนเยาวราช</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/blog8การตกแต่ง.html">8 การตกแต่งสไตล์จีน
-                                                ที่ไม่ใช่เฉพาะคนจีนจะใช้ได้</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/blogพิสดาร.html">ความพิสดารของตะแกรงฉีก!!</a></li>
-                                        <li><a class="dropdown-item"
-                                                href="/BlogAllFolder/ดาวศุภฤกษ์.html">ตกแต่งฟาซาดด้วยลวดลายดาวศุภฤกษ์ให้ความมงคลอย่างไร</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ทับทิม.html">9 ธุรกิจที่เหมาะกับฟาซาด
-                                                ลวดลายทับทิมสยาม</a>
-                                        </li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/อลูมิเนียมทำฟาซาด.html">ใช้อลูมิเนียมทำฟาซาดดีไหม ?</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ฟาซาดสแตนเลท.html">รอดหรือร่วง ฟาซาดสแตนเลส</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ส้มจีน.html">8 เหตุผลทำไมต้องใช้ฟาซาดลวดลายส้มจีน</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/วัสดุสไตล์.html">วัสดุ สไตล์
-                                                แบบไหนที่เข้ากับอาคารธุรกิจทันตกรรม</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ทำฟาซาดโดดเด่น.html">4 วัสดุเลือกใช้ทำฟาซาด โดดเด่น คงทน
-                                                เอกลักษณ์เฉพาะธุรกิจ</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ทำไมต้องวันน์สยาม.html">ทำไมต้องทีมมืออาชีพวันน์สยามเท่านั้น
-                                                ที่สามารถออกแบบ ติดตั้งฟาซาดได้</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ฟาซาดติดทะเล.html">ฟาซาดติดทะเลเลือกวัสดุแบบไหนดี?</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/จักรพรรดิ.html">9 คำตอบ ทำไมฟาซาดลายจักรพรรดิเศรษฐี
-                                                ขึ้นเทรนยอดนิยม</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ได้รับความไว้ใจ.html">เพราะอะไรวันน์สยามได้รับความไว้ใจ
-                                                ตลอดระยะเวลากว่า 10 ปี</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/9ไอเดีย.html">9 ไอเดีย เปลี่ยนอาคารธุรกิจที่ธรรมดา
-                                                ให้ไม่ธรรมดาด้วยฟาซาด</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ปัญหาที่เจ้าของโครง.html">ปัญหาที่เจ้าของโครงการมักพบเจอเมื่อติดตั้งฟาซาด!</a></li>        
-                                        <li><a class="dropdown-item"
-                                                href="/BlogAllFolder/สามกษัตริย์.html">ตกแต่งอาคารด้วยลวดลายสามกษัตริย์อย่างไรให้โดดเด่น ?</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/นกยูงรำแพน.html">ลายตกแต่งนกยูงรำแพน
-                                                ให้ความโดดเด่นและข้อดีอย่างไร ?</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ปัญหาโลกแตก.html">ปัญหาโลกแตก ! อยากตกแต่งฟาซาดลายมงคล
-                                                แต่ลายตกแต่งเยอะเหลือเกิน ใช้แล้วรวยจริง ? หรือจ้อจี้ ?</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/อบฝุ่นpowder.html">9เทคนิคอบสีฝุ่น powder coat
-                                                เพื่อให้ฟาซาดโดดเด่น สะกดทุกสายตา อายุการใช้งานยาวนาน !</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/40ปี.html">เปลี่ยนอพาร์ตเมนต์ 40 ปี ให้ดูใหม่รับปี
-                                                2024!</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ดีไซน์คาเฟ่.html">ดีไซน์คาเฟ่อย่างมีสไตล์
-                                                ด้วยอลูมิเนียมฉีก!</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/4วัสดุเลือกทำฟาซาด.html">4 วัสดุเลือกทำฟาซาด</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/ใช้งานผิดชีวิตเปลียน.html">เลือก(วัสดุ)ใช้งานผิด ชีวิตเปลี่ยน
-                                                เสียหายหลายล้าน</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/7วิธีสำคัญ.html">7วิธีสำคัญ บอกทีมช่าง&สถาปนิก
-                                                ให้ได้ฟาซาดสวยเริ่ดๆตรงใจ</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/สิ่งสำคัญที่ต้องรู้.html">สิ่งสำคัญที่ต้องรู้
-                                                ก่อนเลือกแผ่นเหล็กเจาะรู ทำฟาซาดตกแต่ง</a></li>
-                                        <li><a class="dropdown-item" href="/BlogAllFolder/เลือกตะแกรงฉีก.html">เลือกตะแกรงฉีก
-                                                ทำฟาซาดให้เป็นจากสิ่งเหล่านี้</a></li>
-                                        <li><a class="dropdown-item" href="BlogAllFolder/ดีกว่าเดิม.html">ดีกว่าเดิม
-                                                หากคุณได้ฟาซาดที่ดีในคุณภาพพรีเมียม</a></li>
-                                    </ul>
-        
-                                </li>
-                        
-                    </ul>
-                </div>
+            <!-- <a class="navbar-brand" href="#page-top">One Siame</a>-->
+            <button class="navbar-toggler text-uppercase font-weight-bold bg-primary1 text-white rounded" type="button"
+                data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/admin_panel.php">จัดการข้อมูล</a> 
+                        <!-- ลิงก์ไปยัง admin.php -->
+                    </li>
+                </ul>
+            </div>
         </div>
+
     </nav>
 
     <!-------------------------------------------------------------------------------------------------------------->
@@ -167,7 +66,7 @@
 
 
     </header>
-    
+
     <!--------------------------------------------------------------------------------------------------------------->
     <div class="from_on_top">
         <h1>ข้อมูลในหน้าเว็บ</h1>
