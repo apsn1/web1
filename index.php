5d275ed84486329e6c6812cd03b135bacffa62f1
Xkittipon <kittiponx@gmail.com>
2024-12-16 15:44:57 +0700

    เพิ่มไอค่อน bootstrap

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <link rel="icon" type="image/x-icon" href="/images/logoOld1.png">
     <!-- เพิ่มไฟล์ CSS ของ Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
 
     <!-- เพิ่มไฟล์ JS ของ Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
